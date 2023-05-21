0b23f67354ac9c51ca8e6434f5bb6e4a129ecdfb
C0w14r <132814879+C0w14r@users.noreply.github.com>
2023-05-21 20:55:41 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,218 @@
+<template>
+  <Header :header="this.header" />
+  <div class="content-container">
+    <section class="section-container" id="missions" style="width:435px; height:714px;">
+      <div class="section-header clipped-medium-backward">
+        <img src="/icons/mission-icon.svg" />
+        <h1>Débriefing</h1>
+      </div>
+      <div class="section-content-container">
+        <h3>Opération en cours</h3>
+        <Markdown :source="current_md" class="markdown" />
+        <h3>Liste des opérations</h3>
+        <div class="mission-list-container">
+          <Mission
+            v-for="item in this.missions"
+            :key="item.slug"
+            :mission="item"
+            :selected="this.mission_slug"
+            @click="selectMission(item)"
+          />
+        </div>
+      </div>
+    </section>
+    <section class="section-container" id="events" style="width:435px; height:714px;">
+      <div class="section-header clipped-medium-backward">
+        <img src="/icons/events-icon.svg" />
+        <h1>Journal de bord</h1>
+      </div>
+      <div class="section-content-container">
+        <Markdown :source="events" class="markdown" />
+      </div>
+    </section>
+    <section class="section-container" id="pilots" style="width:894px; height:714px;">
+      <div style="height:52px; overflow:hidden;">
+        <div class="section-header clipped-medium-backward-pilot">
+          <img src="/icons/pilot-icon.svg" />
+          <h1>Personnel</h1>
+        </div>
+        <div class="rhombus-back">&nbsp;</div>
+      </div>
+      <div class="section-content-container">
+        <div class="pilot-list-container">
+          <Pilot v-for="item in this.pilots" :key="item.slug" :pilot="item" />
+        </div>
+      </div>
+    </section>
+  </div>
+  <svg
+    style="visibility: hidden; position: absolute;"
+    width="0"
+    height="0"
+    xmlns="http://www.w3.org/2000/svg"
+    version="1.1"
+  >
+    <defs>
+      <filter id="round">
+        <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
+        <feColorMatrix
+          in="blur"
+          mode="matrix"
+          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -5"
+          result="goo"
+        />
+        <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+      </filter>
+    </defs>
+  </svg>
+  <audio autoplay>
+    <source src="/startup.ogg" type="audio/ogg" />
+  </audio>
+  <Footer/>
+</template>
+
+<script>
+import Header from './components/layout/Header.vue';
+import Footer from './components/layout/Footer.vue';
+import Mission from './components/Mission.vue';
+import Pilot from './components/Pilot.vue';
+import Markdown from 'vue3-markdown-it';
+
+export default {
+  components: {
+    Header,
+    Footer,
+    Mission,
+    Pilot,
+    Markdown
+  },
+
+  data() {
+    return {
+      "mission_slug": "001",
+      "current_md": "",
+      "events": "",
+      "missions": [
+        {
+          "slug": "001",
+          "name": "BAPTÊME DU FEU",
+          "status": "start"
+        },
+      ],
+      "pilots": [
+        {
+          "callsign": "Blitz",
+          "alias": "Sancià Evelisto",
+          "code": "0223c8a6-d600-4f71-ad9c-8ec8b491ede8//NDL-C-LIGHT-GIFT4",
+          "corpro": "GMS",
+          "frame": "Everest",
+          "mech": "Junk Bunk"
+        },
+        {
+          "callsign": "Doc",
+          "alias": "Dann Harbor",
+          "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
+          "corpro": "GMS",
+          "frame": "Everest",
+          "mech": "Basilisk"
+        },
+        {
+          "callsign": "tauron",
+          "alias": "tauron",
+          "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
+          "corpro": "GMS",
+          "frame": "Everest",
+          "mech": "mechatauron"
+        },
+        {
+          "callsign": "Yugo",
+          "alias": "Yugo Burnwood",
+          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
+          "corpro": "GMS",
+          "frame": "Everest",
+          "mech": "Burnwood-01"
+        },
+        {
+          "callsign": "Compadre",
+          "alias": 'Clint "CC" Cruz',
+          "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
+          "corpro": "GMS",
+          "frame": "Everest",
+          "mech": "Rio Bravo"
+        },
+      ],
+      "header": {
+        "planet": "Asperta",
+        "year": "5011u",
+        "system": "Akterea-3",
+        "gate": "Atlas-Akterea",
+        "ring": "Atlas",
+        "headerTitle": "Mirrorsmoke",
+        "headerSubtitle": "Mercenary Company",
+        "subheaderTitle": "Escouade 343",
+        "subheaderSubtitle": "Les Bleu-Bites",
+      },
+      "options":{
+        "eventsMarkdownPerMission": true
+      }
+    }
+  },
+
+  created() {
+    this.loadMissionMarkdown()
+    this.loadEventsMarkdown()
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+    selectMission(mission) {
+      this.mission_slug = mission.slug;
+      this.loadMissionMarkdown()
+      if(this.options.eventsMarkdownPerMission){
+        this.loadEventsMarkdown();
+      }
+    },
+    loadMissionMarkdown() {
+      let self = this;
+      let md = `/missions/${self.mission_slug}.md`
+      var client = new XMLHttpRequest();
+      client.open('GET', md);
+      client.onreadystatechange = function () {
+        self.current_md = client.responseText;
+      }
+      client.send();
+    },
+    loadEventsMarkdown() {
+      let self = this;
+      let md = "";
+
+      if(self.options.eventsMarkdownPerMission){
+        md = `/events/${self.mission_slug}.md`
+      }
+      else {
+        md = "/events.md"
+      }
+
+      var client = new XMLHttpRequest();
+      client.open('GET', md);
+      client.onreadystatechange = function () {
+        self.events = client.responseText;
+      }
+      client.send();
+    }
+  }
+
+}
+</script>
+
+
+<style lang="scss">
+#app {
+  width: 1902px;
+  height: 910px;
+  overflow: hidden;
+}
+</style>
